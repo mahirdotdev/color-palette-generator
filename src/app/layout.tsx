8e1cb6d2f324e9cb68d4a2da50c5bfe083af37bc
Hadie Danker <hadie87@gmail.com>
2025-07-26 21:32:38 +0700

    chore: add eslint disable comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
